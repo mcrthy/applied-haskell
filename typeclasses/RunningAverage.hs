9c4efb3c3ac662c756a0b6e843e1d5872abdbd9f
Matt McCarthy <contact@mattmccarthy.net>
2021-01-11 22:25:06 -0500

    did monoid question

---
+++ typeclasses/RunningAverage.hs
@@ -0,0 +1,43 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-12.21 script
+
+-- EXERCISE: Write a data type for calculating the average of
+-- a bunch of integers. Write Semigroup and Monoid instances for it,
+-- and test them. Define an average function that calculates the
+-- average from these two fields.
+
+{-# LANGUAGE BangPatterns #-}
+
+import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+data RunningTotal =
+  RunningTotal
+  {
+    sum   :: !Int
+  , count :: !Int
+  } deriving (Eq, Show)
+
+instance Arbitrary RunningTotal where
+  arbitrary = do
+    s <- arbitrary
+    c <- arbitrary
+    return (RunningTotal s c)
+
+instance Semigroup RunningTotal where
+  (<>) (RunningTotal s c) (RunningTotal s' c') =
+    RunningTotal (s + s') (c + c')
+
+instance Monoid RunningTotal where
+  mempty = RunningTotal 0 0
+
+instance EqProp RunningTotal where
+  (=-=) = eq
+
+average :: RunningTotal -> Double
+average (RunningTotal s c) = fromIntegral s / fromIntegral c
+
+main :: IO ()
+main = quickBatch $ monoid $ RunningTotal 1 2
